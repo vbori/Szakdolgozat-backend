62ee8097f9e6d98a405d8eea05095691c72a5366
Varga Bori <vx53gk@inf.elte.hu>
2023-02-17 20:09:01 +0100

    Adding JWT authorization

--- routes/research.js
+++ routes/research.js
@@ -6,6 +6,10 @@ const jwt = require('jsonwebtoken');
 
 const Researcher = require('../models/researcher');
 
+router.get('/', authenticateToken, async (req, res) => {
+    res.send(req.user.name)
+});
+
 router.post('/register', async (req, res) => {
     try {
         let newResearcher = new Researcher({
@@ -36,13 +40,27 @@ router.post('/login', async (req, res) => {
 
     try {
         if(await bcrypt.compare(req.body.password, researcher.password)) {
-            res.send('Success')
+            const user = {name: req.body.username}
+            const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET)
+            res.json({accessToken: accessToken})
         } else {
             res.send('Not Allowed')
         }
     } catch {
         res.status(500).send()
     }
-  })
+});
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if(err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
 
 module.exports = router;
